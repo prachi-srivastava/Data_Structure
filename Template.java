68088fd5be26360da1650167419e4ec89f972136
prachi-srivastava <prachi.hbtik@gmail.com>
2021-11-09 08:18:56 +0530

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,39 @@
+Hash Map Questions Template
+======================================
+
+
+ReturnType aggregateByKey_hashmap(List<Type>& keys) {
+    // Replace Type and InfoType with actual type of your key and value
+    Map<Type, InfoType> hashmap = new HashMap<>();
+    for (Type key : keys) {
+        if (hashmap.containsKey(key)) {
+            if (hashmap.get(key) satisfies the requirement) {
+                return needed_information;
+            }
+        }
+        // Value can be any information you needed (e.g. index)
+        hashmap.put(key, value);    
+    }
+    return needed_information;
+}
+
+
+
+
+
+/*
+ * Template for using hash map to find duplicates.
+ * Replace ReturnType with the actual type of your return value.
+ */
+ReturnType aggregateByKey_hashmap(List<Type>& keys) {
+    // Replace Type and InfoType with actual type of your key and value
+    Map<Type, InfoType> hashmap = new HashMap<>();
+    for (Type key : keys) {
+        if (hashmap.containsKey(key)) {
+            hashmap.put(key, updated_information);
+        }
+        // Value can be any information you needed (e.g. index)
+        hashmap.put(key, value);    
+    }
+    return needed_information;
+}
